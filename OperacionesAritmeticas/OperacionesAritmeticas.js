3086258f143c905011d6001c4e5eb8a72c72da4e
Enrique Terrero <enriqueterrero1006@gmail.com>
2022-04-29 10:48:22 -0400

    OperacionesAritmeticas

---
+++ OperacionesAritmeticas/OperacionesAritmeticas.js
@@ -0,0 +1,9 @@
+var numero = 100;
+//numero +numero+10;
+//numero+=10;
+//numero = (numero+10-5)*2/4;
+//alert(numero);
+var peso = prompt("Ingrese su peso");
+var estatura = prompt("Ingrese su estatura");
+var resultado = peso/estatura;
+alert("Su indice de masa corporal es  "+ resultado);
