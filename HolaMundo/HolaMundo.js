fea67a2058bf4828d38ff9e19513ff5daa754710
Enrique Terrero <enriqueterrero1006@gmail.com>
2022-04-29 10:24:29 -0400

    HolaMundo

---
+++ HolaMundo/HolaMundo.js
@@ -0,0 +1,6 @@
+
+var nombre = prompt("Ingrese el nombre");
+var edad = prompt("ingrese su edad");
+var telefono=prompt("ingrese su telefono");
+var mensajefinal = "nombre: "+nombre+"edad: "+edad+"telefono "+telefono;
+alert(mensajefinal);
