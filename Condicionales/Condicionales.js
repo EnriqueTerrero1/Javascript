c74522c15c512c9b4ca0664fb73fa47a7b0bb7e8
Enrique Terrero <enriqueterrero1006@gmail.com>
2022-04-29 11:01:40 -0400

    Condiciones

---
+++ Condicionales/Condicionales.js
@@ -0,0 +1,10 @@
+var edad = prompt("Ingrese su edad");
+var estatura = prompt("Ingrese su estatura en metros");
+
+if(edad >=18 && estatura >1.2){
+
+    alert("usted puede entrar a el juego");
+}else
+{
+    alert("usted es menor de edad su edad es  "+ edad);
+}
